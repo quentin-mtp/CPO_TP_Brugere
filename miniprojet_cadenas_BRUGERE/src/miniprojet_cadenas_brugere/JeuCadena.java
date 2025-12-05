809aaba719022f6b81cd789fe9348a5c72eba606
quentin brugere <quentin34.brugere@gmail.com>
2025-12-05 17:19:51 +0100

    Tp1_ManipNombresInt_BRUGERE fin seancce 1

---
+++ miniprojet_cadenas_BRUGERE/src/miniprojet_cadenas_brugere/JeuCadena.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package miniprojet_cadenas_brugere;
+import java.util.Random;
+import java.util.Arrays;
+        
+/**
+ *
+ * @author quent
+ */
+public class JeuCadena {
+    private  int[] codeSecret = new int[4];
+    private int tentativesUtilisees =0;
+    private  int MAX_TENTATIVES = 5;
+}
