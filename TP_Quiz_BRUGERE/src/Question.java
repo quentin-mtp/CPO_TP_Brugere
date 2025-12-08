9d59a873c76ebd5c2130badd3014dbb37013250c
quentin brugere <quentin34.brugere@gmail.com>
2025-12-08 17:24:56 +0100

    Tp1_ManipNombresInt_BRUGERE finin normalement enfinnnnnnnn!

---
+++ TP_Quiz_BRUGERE/src/Question.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author quent
+ */
+public class Question {
+private String intitule;
+private String proposition1;
+private String proposition2; 
+private String proposition3;
+private String proposition4;
+private int indexBonneReponse;
+
+public Question(String intitule, String p1, String p2, String p3, String
+p4, int indexBonneReponse) {
+this.intitule  = intitule;
+this.proposition1= p1;
+this.proposition2=p2;
+this.proposition3=p3;
+this.proposition4=p4;
+this.indexBonneReponse= indexBonneReponse;
+}
+public String getIntitule(){
+return intitule;}
+public String getproposition1(){
+return proposition1;
+}
+public String getproposition2(){
+return proposition2;
+}
+public String getpropostion3(){
+return proposition3;
+}
+public String getproposition4(){
+return proposition4;
+}
+public int getindexBonneReponse(){
+return indexBonneReponse;}
+public boolean estBonneReponse(int indexChoisi) {
+    return indexChoisi == indexBonneReponse;
+}
+
+}
+
+
+
