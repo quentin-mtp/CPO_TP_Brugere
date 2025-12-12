c4083a6857af8292b2789d4fd733f9fa843c21a8
quentin brugere <quentin34.brugere@gmail.com>
2025-12-12 16:06:27 +0100

    suppression d'une fonction métier en trop et réglale d'un autre problème de calcul

--- miniprojet_cadenas_BRUGERE/src/miniprojet_cadenas_brugere/Cadenas.java
+++ miniprojet_cadenas_BRUGERE/src/miniprojet_cadenas_brugere/Cadenas.java
@@ -9,7 +9,7 @@ package miniprojet_cadenas_brugere;
  * @author quent
  */
 public class Cadenas extends javax.swing.JFrame {
-    
+    JeuCadena maPartie = new JeuCadena();
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(Cadenas.class.getName());
 
     /**
@@ -182,43 +182,130 @@ public class Cadenas extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void up_chiffre_1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_up_chiffre_1ActionPerformed
-        // TODO add your handling code here:
+               String texte= texte_chiffre_1.getText();
+        int nombre = Integer.parseInt(texte);
+        nombre +=1;
+        if(nombre<0){
+        nombre=9;
+        }
+        texte_chiffre_1.setText(String.valueOf(nombre));
     }//GEN-LAST:event_up_chiffre_1ActionPerformed
 
     private void up_chiffre_2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_up_chiffre_2ActionPerformed
-        // TODO add your handling code here:
+         String texte= texte_chiffre_2.getText();
+        int nombre = Integer.parseInt(texte);
+        nombre +=1;
+        if(nombre<0){
+        nombre=9;
+        }
+        texte_chiffre_2.setText(String.valueOf(nombre));
     }//GEN-LAST:event_up_chiffre_2ActionPerformed
 
     private void up_chiffre_3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_up_chiffre_3ActionPerformed
-        // TODO add your handling code here:
+        String texte= texte_chiffre_3.getText();
+        int nombre = Integer.parseInt(texte);
+        nombre +=1;
+        if(nombre<0){
+        nombre=9;
+        }
+        texte_chiffre_3.setText(String.valueOf(nombre));
     }//GEN-LAST:event_up_chiffre_3ActionPerformed
 
     private void up_chiffre_4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_up_chiffre_4ActionPerformed
-        // TODO add your handling code here:
+      String texte= texte_chiffre_4.getText();
+        int nombre = Integer.parseInt(texte);
+        nombre +=1;
+        if(nombre<0){
+        nombre=9;
+        }
+        texte_chiffre_4.setText(String.valueOf(nombre));
     }//GEN-LAST:event_up_chiffre_4ActionPerformed
 
     private void down_chiffre_1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_down_chiffre_1ActionPerformed
-        // TODO add your handling code here:
+              String texte= texte_chiffre_1.getText();
+        int nombre = Integer.parseInt(texte);
+        nombre -=1;
+        if(nombre<0){
+        nombre=9;
+        }
+        texte_chiffre_1.setText(String.valueOf(nombre));
     }//GEN-LAST:event_down_chiffre_1ActionPerformed
 
     private void down_chiffre_2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_down_chiffre_2ActionPerformed
-        // TODO add your handling code here:
+                String texte= texte_chiffre_2.getText();
+        int nombre = Integer.parseInt(texte);
+        nombre -=1;
+        if(nombre<0){
+        nombre=9;
+        }
+        texte_chiffre_2.setText(String.valueOf(nombre));
     }//GEN-LAST:event_down_chiffre_2ActionPerformed
 
     private void down_chiffre_3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_down_chiffre_3ActionPerformed
-        // TODO add your handling code here:
+               String texte= texte_chiffre_3.getText();
+        int nombre = Integer.parseInt(texte);
+        nombre -=1;
+        if(nombre<0){
+        nombre=9;
+        }
+        texte_chiffre_3.setText(String.valueOf(nombre));
     }//GEN-LAST:event_down_chiffre_3ActionPerformed
 
     private void down_chiffre_4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_down_chiffre_4ActionPerformed
-        // TODO add your handling code here:
+        String texte= texte_chiffre_4.getText();
+        int nombre = Integer.parseInt(texte);
+        nombre -=1;
+        if(nombre<0){
+        nombre=9;
+        }
+        texte_chiffre_4.setText(String.valueOf(nombre));
     }//GEN-LAST:event_down_chiffre_4ActionPerformed
 
     private void bouton_testerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bouton_testerActionPerformed
-        // TODO add your handling code here:
+        int[] proposition = new int[4];
+
+        proposition[0] = Integer.parseInt(texte_chiffre_1.getText());
+        proposition[1] = Integer.parseInt(texte_chiffre_2.getText());
+        proposition[2] = Integer.parseInt(texte_chiffre_3.getText());
+        proposition[3] = Integer.parseInt(texte_chiffre_4.getText());
+
+        int[] resultat = maPartie.verifierCombinaison(proposition);
+
+        texte_nb_chiffres_exacts.setText(String.valueOf(resultat[0]));
+
+        texte_nb_chiffres_haut.setText(String.valueOf(resultat[1]));
+
+        texte_nb_chiffres_bas.setText(String.valueOf(resultat[2]));
+
+        int essais = maPartie.getTentativeActuelle();
+        int max = maPartie.getMaxTentatives();
+        texte_tentatives.setText(essais + " sur " + max);
+
+        if (resultat[0] == 4) {
+            javax.swing.JOptionPane.showMessageDialog(this, "Bravo ! Vous avez trouvé le code !");
+            bouton_tester.setEnabled(false);
+        } else if (essais >= max) {
+            javax.swing.JOptionPane.showMessageDialog(this, "Perdu ! Retentez votre chance.");
+            bouton_tester.setEnabled(false);
+        }
+                             
     }//GEN-LAST:event_bouton_testerActionPerformed
 
     private void bouton_recommencerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bouton_recommencerActionPerformed
-        // TODO add your handling code here:
+      maPartie.initialiserJeuCadena();
+
+        texte_chiffre_1.setText("0");
+        texte_chiffre_2.setText("0");
+        texte_chiffre_3.setText("0");
+        texte_chiffre_4.setText("0");
+
+        texte_nb_chiffres_exacts.setText("0");
+        texte_nb_chiffres_haut.setText("0");
+        texte_nb_chiffres_bas.setText("0");
+
+        texte_tentatives.setText("0 sur " + maPartie.getMaxTentatives());
+
+        bouton_tester.setEnabled(true);
     }//GEN-LAST:event_bouton_recommencerActionPerformed
 
     /**
