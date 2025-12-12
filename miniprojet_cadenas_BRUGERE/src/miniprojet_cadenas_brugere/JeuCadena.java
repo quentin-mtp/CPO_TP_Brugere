7b739ecc45b32cc7c8c79b926ab8c2b722821e1e
quentin brugere <quentin34.brugere@gmail.com>
2025-12-12 15:59:20 +0100

    Réglage d'un problème qui ne faisait lancer le jeux qu'une seule fois

--- miniprojet_cadenas_BRUGERE/src/miniprojet_cadenas_brugere/JeuCadena.java
+++ miniprojet_cadenas_BRUGERE/src/miniprojet_cadenas_brugere/JeuCadena.java
@@ -4,14 +4,54 @@
  */
 package miniprojet_cadenas_brugere;
 import java.util.Random;
-import java.util.Arrays;
+
         
 /**
  *
  * @author quent
  */
 public class JeuCadena {
-    private  int[] codeSecret = new int[4];
-    private int tentativesUtilisees =0;
-    private  int MAX_TENTATIVES = 5;
+    private int[] code;
+    private int tentative;
+    private int tentative_max;
+    
+public JeuCadena(){
+code= new int[4];
+initialiserJeuCadena();
 }
+public void initialiserJeuCadena(){
+Random Rand = new Random();
+tentative=0;
+for (int i=0; i<4; i++){
+code[i]=Rand.nextInt(10);
+}
+  System.out.println("CODE SECRET: "+code[0]+code[1]+code[2]+code[3]);     
+}
+public int[] verifierCombinaison(int[] proposition) {
+        int exacts = 0;
+        int thaut = 0;
+        int tbas = 0;
+       
+        for (int i = 0; i < 4; i++) {
+            if (proposition[i] == code[i]) {
+                exacts++;
+            } else if (proposition[i] > code[i]) {
+                thaut++;
+            } else {
+                tbas++;
+            }
+        }
+       
+        tentative ++;
+        return new int[]{exacts, thaut, tbas};
+    }
+   
+    public int getTentativeActuelle() {
+        return tentative;
+    }
+   
+    public int getMaxTentatives() {
+        return tentative_max;
+    }
+}
+
